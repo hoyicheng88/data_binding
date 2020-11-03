6f313e001faccffe46af4ae3ec8ef26a7297298d
Starmy <mf99250@gmail.com>
2020-11-03 15:41:04 +0800

    Initial Commit

---
+++ data_binding/data_bindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  data_bindingApp.swift
+//  data_binding
+//
+//  Created by User19 on 2020/11/3.
+//
+
+import SwiftUI
+
+@main
+struct data_bindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
